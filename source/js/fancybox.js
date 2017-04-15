08475cd8ccbb78ee0f99f7f1de93ff7b3477b84e
hscpro <huisc@foxmail.com>
2017-04-15 10:31:55 +0800

    同步hscbook

--- source/js/fancybox.js
+++ source/js/fancybox.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     if ($(this).parent().hasClass('fancybox')) return;
     if ($(this).hasClass('nofancybox')) return;
     var alt = this.alt;
-    if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+	// 关闭ALT显示
+    //if (alt) $(this).after('<span class="caption">' + alt + '</span>');
     $(this).wrap('<a href="' + ($(this).attr('data-src') == null ? this.src : $(this).attr('data-src')) + '" title="' + alt + '" class="fancybox"></a>');
   });
   $(this).find('.fancybox').each(function(){
