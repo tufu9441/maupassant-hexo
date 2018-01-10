56d2827af9b8ff94dd1be030337a97ef4efd6f0d
bruce@ <350108081@qq.com>
2018-01-10 08:01:33 +0800

    donate bug fix

--- source/js/donate.js
+++ source/js/donate.js
@@ -53,7 +53,7 @@ jQuery(document).ready(function() {
 		});
 	}
 
-	$('#donateBox>li').click(function(event) {
+	$('#donateBox li').click(function(event) {
 		var thisID = $(this).attr('id');
 		if(thisID === 'BTC') {
 			showQR(BTCQR);
